d07efb775fbc195398954fe8f041e0e2cda2e008
Vignesh V <vigneshvijayan103@gmail.com>
2025-11-27 14:15:16 +0530

    Enabled Swagger in production

--- Program.cs
+++ Program.cs
@@ -44,16 +44,11 @@ app.UseCors();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
